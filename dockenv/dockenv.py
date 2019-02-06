f5fe89a8fc45b464609a8188b8f3b39eb54c6843
pathtofile <path.to.file@gmail.com>
2019-02-06 12:53:01 +1100

    added export, import, and upgrade functions. Removed scripts folder

--- dockenv/dockenv.py
+++ dockenv/dockenv.py
@@ -13,7 +13,6 @@ import shlex
 import docker
 
 ROOT_FOLDER = os.path.abspath(os.path.dirname(__file__))
-CONTAINER_ROOT = os.path.join(ROOT_FOLDER, "scripts")
 CLIENT = docker.from_env()
 
 LOGGER = logging.getLogger(__name__)
@@ -156,25 +155,56 @@ def run_script(dockenv_name,
                          "Use 'dockenv run --verbose' to get more info")
 
 
-def func_new_venv(args):
+def build_venv(args, upgrade=False):
     """
-    Create a new virtual env. This will build a Docker image based on the
-    "Python:3" image. Our Image will be named named "dockenv-<envname>"
+    Create a new virtual env or upgrade an existing one. If new, this will
+    build a Docker image based on the "Python:3" image, and our Image will
+    be named named "dockenv-<envname>". If upgrading, we will build from a
+    base image of the same name
 
-    :param args: cli arguments
+    :param args: cli args
+    :param upgrade: If True, base image will be the same dockenv image.
+                   If False, base image will be "python:3"
     """
     dockenv_name = f"dockenv-{args.envname}"
-    # Check env doesn't already exist:
-    if local_image_exists(dockenv_name):
+
+    image_exists = local_image_exists(dockenv_name)
+    # Check if either upgrading and image is missing,
+    # or if creating new and image already exists
+    if (not upgrade) and image_exists:
         LOGGER.error(f"ERROR: Virtual Env {args.envname!r} already exists! "
                      "Use 'dockenv delete' or 'dockenv run'")
         return
+    if upgrade and (not image_exists):
+        LOGGER.error(f"ERROR: Virtual Env {args.envname!r} doesn't exist! ")
+        return
+
+    if args.package and args.requirements:
+        LOGGER.error(f"ERROR: Use only one of '--package' or '--requirements'")
+        return
+
+    if upgrade:
+        base_image = dockenv_name
+    else:
+        base_image = "python:3"
+
+    pip_script = ""
+    if args.requirements or args.package:
+        pip_script = "RUN pip install --no-cache-dir -r requirements.txt"
+
+    dockerfile = f"""
+    FROM {base_image}
+    WORKDIR /usr/src/app
+    COPY . .
+    {pip_script}
+    CMD [ "sh", "./runner/run.sh" ]
+    """
 
     # Put everything inside a temp directory
     with tempfile.TemporaryDirectory() as build_dir:
         # copy required files
-        shutil.copy(os.path.join(CONTAINER_ROOT, "Dockerfile"), build_dir)
-        shutil.copy(os.path.join(CONTAINER_ROOT, "build.py"), build_dir)
+        with open(os.path.join(build_dir, "Dockerfile"), "w") as fdockerfile:
+            fdockerfile.write(dockerfile)
 
         # Copy any optional files
         if args.requirements is not None:
@@ -182,7 +212,8 @@ def func_new_venv(args):
                         os.path.join(build_dir, "requirements.txt"))
 
         if args.package is not None:
-            with open(os.path.join(build_dir, "package.txt"), "w") as fpackage:
+            with open(os.path.join(build_dir, "requirements.txt"),
+                      "w") as fpackage:
                 fpackage.write(args.package)
 
         # It takes a while to build the base Python3 image if we haven't already
@@ -194,6 +225,26 @@ def func_new_venv(args):
         LOGGER.info(f"[*] building virtual env {dockenv_name}...")
         CLIENT.images.build(tag=dockenv_name, path=build_dir)
         LOGGER.info(f"[*] built virtual env {dockenv_name}")
+
+
+def func_new_venv(args):
+    """
+    Create a new virtual env. This will build a Docker image based on the
+    "Python:3" image. Our Image will be named named "dockenv-<envname>"
+
+    :param args: cli arguments
+    """
+    build_venv(args, upgrade=False)
+
+
+def func_upgrade_venv(args):
+    """
+    Upgrade a virtual env, installing new packages and creating
+    a new version of the virtualenv image
+
+    :param args: cli arguments
+    """
+    build_venv(args, upgrade=True)
 
 
 def func_run_script(args):
@@ -271,11 +322,58 @@ def func_delete_venv(args):
     CLIENT.images.remove(dockenv_name)
 
 
+def func_export_venv(args):
+    """
+    Exports a virtual environment to a .tar file
+
+    :param args: cli arguments
+    """
+    dockenv_name = f"dockenv-{args.envname}"
+
+    # Check env exists:
+    if not local_image_exists(dockenv_name):
+        LOGGER.error(f"ERROR: Virtual Env {args.envname!r} doesn't exist")
+        return
+
+    image = CLIENT.images.get(dockenv_name)
+    LOGGER.info(f"Exporting env {args.envname!r}, this can take 5-10 minutes")
+    with open(args.filename, "wb") as fimage:
+        for chunk in image.save(chunk_size=209715, named=True):
+            fimage.write(chunk)
+    LOGGER.info(f"Exported env {args.envname!r} to {args.filename!r}")
+
+
+def func_import_venv(args):
+    """
+    Imports a virtual environment from a saved a .tar file
+
+    :param args: cli arguments
+    """
+    LOGGER.info(
+        f"Attempting to import from {args.filename!r}, this can take 5 minutes"
+    )
+    with open(args.filename, "rb") as fimage:
+        image = CLIENT.images.load(fimage.read())[0]
+    # Get the new env name
+    for tag in image.tags:
+        if tag.startswith("dockenv"):
+            LOGGER.info(f"Imported env {get_venv_name(tag)!r}")
+            break
+    else:
+        # Wasn't a dockenv image, remove it an error out
+        CLIENT.images.remove(image.id)
+        LOGGER.error(
+            "Imported Docker image from {args.filename!r} wasn'y a dockenv env"
+        )
+
+
 def main():
     """
     Main entry function
     """
     parser = argparse.ArgumentParser(description="Run python inside docker")
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="print verbose output")
     subparsers = parser.add_subparsers(help="options")
 
     # --- New virtual Env ---
@@ -286,9 +384,20 @@ def main():
         "--requirements", "-r", help="requirements.txt file to install first")
     new_parser.add_argument(
         "--package", "-p", help="name of a packge to install from pip first")
-    new_parser.add_argument(
-        "-v", "--verbose", action="store_true", help="print verbose output")
     new_parser.set_defaults(func=func_new_venv)
+
+    # --- Upgrade virtual Env ---
+    upgrade_parser = subparsers.add_parser(
+        "upgrade", help="upgrade an existing virtual environment")
+    upgrade_parser.add_argument(
+        "envname", help="name of the virtualenv to upgrade")
+    upgrade_parser.add_argument(
+        "--requirements",
+        "-r",
+        help="requirements.txt file to install into virtualenv")
+    upgrade_parser.add_argument(
+        "--package", "-p", help="name of a packge to install into virtualenv")
+    upgrade_parser.set_defaults(func=func_upgrade_venv)
 
     # --- Run Script ---
     run_parser = subparsers.add_parser(
@@ -296,8 +405,6 @@ def main():
     run_parser.add_argument(
         "envname", help="name of the virtualenv to run script in")
     run_parser.add_argument("script", help="script to run")
-    run_parser.add_argument(
-        "-v", "--verbose", action="store_true", help="print verbose output")
     run_parser.add_argument(
         "-e",
         "--expose-port",
@@ -317,8 +424,6 @@ def main():
     # --- List Virtual Envs ---
     list_parser = subparsers.add_parser(
         "list", help="list all virtual environments")
-    list_parser.add_argument(
-        "-v", "--verbose", action="store_true", help="print verbose output")
     list_parser.set_defaults(func=func_list_venv)
 
     # --- List packages inside a Virtual Env ---
@@ -326,17 +431,29 @@ def main():
         "freeze", help="list packages inside an environment")
     freeze_parser.add_argument(
         "envname", help="name of the virtualenv to list packges in")
-    freeze_parser.add_argument(
-        "-v", "--verbose", action="store_true", help="print verbose output")
     freeze_parser.set_defaults(func=func_freeze_venv)
 
     # --- Delete Virtual Env ---
     del_parser = subparsers.add_parser(
-        "delete", help="delete a virtual environment")
+        "delete", aliases=['del'], help="delete a virtual environment")
     del_parser.add_argument("envname", help="name of the virtualenv to enter")
-    del_parser.add_argument(
-        "-v", "--verbose", action="store_true", help="print verbose output")
     del_parser.set_defaults(func=func_delete_venv)
+
+    # --- Export Virtual Env ---
+    export_parser = subparsers.add_parser(
+        "export", help="Exports a virtual environment into a .tar file")
+    export_parser.add_argument(
+        "envname", help="name of the virtualenv to export")
+    export_parser.add_argument(
+        "filename", help="file to save the virtualenv to")
+    export_parser.set_defaults(func=func_export_venv)
+
+    # --- Import Virtual Env ---
+    import_parser = subparsers.add_parser(
+        "import", help="imports a virtual environment from a .tar file")
+    import_parser.add_argument(
+        "filename", help="file to save the virtualenv to")
+    import_parser.set_defaults(func=func_import_venv)
 
     if len(sys.argv) == 1:
         parser.print_help()
